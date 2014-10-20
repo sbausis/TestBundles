05c611cc13fe218de01a21d754d76c722385732e
sbausis <sbausis@gmx.net>
2014-10-20 14:25:18 +0200

    Initial Commit

---
+++ BundleLoader/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BundleLoader
+//
+//  Created by Simon Pascal Baur on 20/10/14.
+//  Copyright (c) 2014 Simon Pascal Baur. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ BundleLoader/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  BundleLoader
+//
+//  Created by Simon Pascal Baur on 20/10/14.
+//  Copyright (c) 2014 Simon Pascal Baur. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
